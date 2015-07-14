af5a48b5c76185d8a604b72ec7a71060eefa1a16
fuzzylikeagangster <fuzzylikeagangster@yahoo.com>
2015-07-14 15:16:24 -0400

    Tweak scripts

--- class/build.cmd
+++ class/build.cmd
@@ -3,7 +3,7 @@ dir
 
 REM COMPILE JAVA CLASSES
 REM "c:\program files\java\jdk1.8.0_45\bin\javac.exe" -d ../class ../src/*.java
-"c:\program files\java\jdk1.8.0_45\bin\javac.exe" ../src/*.java
+"c:\program files\java\jdk1.8.0_45\bin\javac.exe" -d . ../src/*.java
 
 REM COMBINE JAVA CLASS FILES INTO AN EXECUTABLE JAR FILE
 "c:\program files\java\jdk1.8.0_45\bin\jar.exe" cmfv MANIFEST.MF SillyApp.jar MyPackage/*.class
--- class/testbuild.cmd
+++ class/testbuild.cmd
@@ -1,7 +1,8 @@
 dir
 
 REM COMPILE JAVA TEST CLASSES
-"c:\program files\java\jdk1.8.0_45\bin\javac.exe" -cp .;C:\Tools\junit-4.12\junit-4.12.jar -d ../class ../test/*.java
+"c:\program files\java\jdk1.8.0_45\bin\javac.exe" -cp .;C:\Tools\junit-4.12\junit-4.12.jar -d . ../test/*.java
 
 REM COMBINE JAVA CLASS FILES INTO AN EXECUTABLE JAR FILE
 "c:\program files\java\jdk1.8.0_45\bin\jar.exe" cmfv MANIFEST.MF SillyAppTests.jar MyPackage/*.class MyPackage/Tests/*.class
+
