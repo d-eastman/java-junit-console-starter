5db8b2ca9bd8cd8c060324cfea3efa43a8987534
fuzzylikeagangster <fuzzylikeagangster@yahoo.com>
2015-07-14 14:55:38 -0400

    Tweak batch files

--- class/build.cmd
+++ class/build.cmd
@@ -2,7 +2,8 @@ dir
 
 
 REM COMPILE JAVA CLASSES
-"c:\program files\java\jdk1.8.0_45\bin\javac.exe" -d ../class ../src/*.java
+REM "c:\program files\java\jdk1.8.0_45\bin\javac.exe" -d ../class ../src/*.java
+"c:\program files\java\jdk1.8.0_45\bin\javac.exe" ../src/*.java
 
 REM COMBINE JAVA CLASS FILES INTO AN EXECUTABLE JAR FILE
 "c:\program files\java\jdk1.8.0_45\bin\jar.exe" cmfv MANIFEST.MF SillyApp.jar MyPackage/*.class
--- class/testbuild.cmd
+++ class/testbuild.cmd
@@ -1,3 +1,5 @@
+dir
+
 REM COMPILE JAVA TEST CLASSES
 "c:\program files\java\jdk1.8.0_45\bin\javac.exe" -cp .;C:\Tools\junit-4.12\junit-4.12.jar -d ../class ../test/*.java
 
